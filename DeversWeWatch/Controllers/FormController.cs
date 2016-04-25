38cc1bfeae7f367e38040a6c24b30e32195f0acc
Patrik Nilsson <patteee@gmail.com>
2016-04-25 10:37:06 +0200

    Lagt till en controller för att ta emot data från formuläret

---
+++ DeversWeWatch/Controllers/FormController.cs
@@ -0,0 +1,39 @@
+﻿using DeversWeWatch.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DeversWeWatch.Controllers
+{
+    
+    public class FormController : Controller
+    {
+        ReportContext context = new ReportContext();
+        public ActionResult Index ()
+        {
+            return View();
+        }
+       [HttpPost]
+       public ActionResult Index(ReportModel modelInput)
+        {
+            var report = new ReportModel()
+            {
+                Datum = modelInput.Datum,
+                Efternamn = modelInput.Efternamn,
+                Email = modelInput.Email,
+                Förnamn = modelInput.Förnamn,
+                Kategori = modelInput.Kategori,
+                Meddelande = modelInput.Meddelande,
+                Personnummer = modelInput.Personnummer,
+                Plats_Adress = modelInput.Plats_Adress,
+                Telefonnummer = modelInput.Telefonnummer
+            };
+            
+            context.Report.Add(report);
+            context.SaveChanges();
+            return View();
+        }
+    }
+}
