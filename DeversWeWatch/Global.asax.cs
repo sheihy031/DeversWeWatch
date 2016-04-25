e0c7dd09eb77b9f47f795fb61a000b5469afdd51
Malin Classon <m.classon@hotmail.com>
2016-04-25 13:40:09 +0200

    Added migration for initial_create. Added in Appllication_start. Form works without exceptions bbut still issues with radiobuttons.

--- DeversWeWatch/Global.asax.cs
+++ DeversWeWatch/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Data.Entity;
 
 namespace DeversWeWatch
 {
@@ -13,6 +14,8 @@ namespace DeversWeWatch
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            Database.SetInitializer<ReportContext>(null);
         }
     }
 }
--- DeversWeWatch/Migrations/201604221345301_InitialCreate.cs
+++ DeversWeWatch/Migrations/201604221345301_InitialCreate.cs
@@ -7,28 +7,20 @@ namespace DeversWeWatch.Migrations
     {
         public override void Up()
         {
-            CreateTable(
-                "dbo.ReportModels",
-                c => new
-                    {
-                        Id = c.Int(nullable: false, identity: true),
-                        Förnamn = c.String(),
-                        Efternamn = c.String(),
-                        Email = c.String(),
-                        Telefonnummer = c.String(),
-                        Personnummer = c.String(maxLength: 10),
-                        Datum = c.DateTime(nullable: false),
-                        Plats_Adress = c.String(nullable: false),
-                        Kategori = c.Int(nullable: false),
-                        Meddelande = c.String(nullable: false),
-                    })
-                .PrimaryKey(t => t.Id);
-            
+            AlterColumn("dbo.ReportModels", "Email", c => c.String(maxLength: 75));
+            AlterColumn("dbo.ReportModels", "Telefonnummer", c => c.String(maxLength: 50));
+            AlterColumn("dbo.ReportModels", "Personnummer", c => c.String());
+            AlterColumn("dbo.ReportModels", "Plats_Adress", c => c.String(nullable: false, maxLength: 50));
+            AlterColumn("dbo.ReportModels", "Meddelande", c => c.String(nullable: false, maxLength: 128));
         }
         
         public override void Down()
         {
-            DropTable("dbo.ReportModels");
+            AlterColumn("dbo.ReportModels", "Meddelande", c => c.String(nullable: false));
+            AlterColumn("dbo.ReportModels", "Plats_Adress", c => c.String(nullable: false));
+            AlterColumn("dbo.ReportModels", "Personnummer", c => c.String(maxLength: 10));
+            AlterColumn("dbo.ReportModels", "Telefonnummer", c => c.String());
+            AlterColumn("dbo.ReportModels", "Email", c => c.String());
         }
     }
 }
